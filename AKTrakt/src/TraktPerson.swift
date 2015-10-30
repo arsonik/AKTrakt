d7859e68e898e8878000f0fa8006ee4d24aff490
Florian Morello <arsonik@me.com>
2015-10-30 10:07:17 -0400

    cleanup

--- AKTrakt/src/TraktPerson.swift
+++ AKTrakt/src/TraktPerson.swift
@@ -26,36 +26,4 @@ class TraktPerson : TraktObject {
     }
 }
 
-class TraktCharacter {
-    let character:String!
-    let person:TraktPerson!
 
-    init?(data: [String : AnyObject]!) {
-        if let n = data["character"] as? String, p = data["person"] as? [String:AnyObject], pers = TraktPerson(data: p) {
-            character = n
-            person = pers
-        }
-        else {
-            character = nil
-            person = nil
-            return nil
-        }
-    }
-}
-
-class TraktCrew {
-    let job:String!
-    let person:TraktPerson!
-
-    init?(data: [String : AnyObject]!) {
-        if let n = data["job"] as? String, p = data["person"] as? [String:AnyObject], pers = TraktPerson(data: p) {
-            job = n
-            person = pers
-        }
-        else {
-            job = nil
-            person = nil
-            return nil
-        }
-    }
-}
--- Podfile
+++ Podfile
@@ -1,4 +1,3 @@
 use_frameworks!
 
 pod "Alamofire"
-pod "Async"
